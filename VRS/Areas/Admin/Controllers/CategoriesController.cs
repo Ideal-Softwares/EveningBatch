5ff500d29c569ce2e54d94c65de3b7b55963a2de
Swapnil Kunkekar <swapnil93kunkekar@gmail.com>
2024-08-08 21:49:09 +0530

    Added Category and Vehicle Controller

--- VRS/Areas/Admin/Controllers/CategoriesController.cs
+++ VRS/Areas/Admin/Controllers/CategoriesController.cs
@@ -3,34 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using VRS.Areas.Admin.Models;
-using VRS.Areas.Admin.Models.VM;
 using VRS.Data;
 
 namespace VRS.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize]
-    public class BrandsController : Controller
+    public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
 
-        public BrandsController(ApplicationDbContext context)
+        public CategoriesController(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        // GET: Admin/Brands
+        // GET: Admin/Categories
         public async Task<IActionResult> Index()
         {
-            return View(await _context.brands.Select(b => new BrandVM { BrandName = b.BrandName, Id = b.Id }).ToListAsync());
+            return View(await _context.categories.ToListAsync());
         }
 
-        // GET: Admin/Brands/Details/5
+        // GET: Admin/Categories/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -38,52 +35,44 @@ namespace VRS.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var brand = await _context.brands
+            var category = await _context.categories
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (brand == null)
+            if (category == null)
             {
                 return NotFound();
             }
 
-            return View(brand);
+            return View(category);
         }
 
-        // GET: Admin/Brands/Create
+        // GET: Admin/Categories/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Admin/Brands/Create
+        // POST: Admin/Categories/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,BrandName")] BrandVM brand)
+        public async Task<IActionResult> Create([Bind("Id,CategoryName,Status,CreatedBy,CreatedTime,UpdatedBy,UpdatedTime")] Category category)
         {
             if (ModelState.IsValid)
             {
                 // For ASP.NET Core >= 5.0
                 var Userid = User.FindFirstValue(ClaimTypes.NameIdentifier);// will give the user's Id
+                category.CreatedBy = new Guid(Userid);
+                category.CreatedTime = DateTime.Now;
 
-                Brand b = new Brand
-                {
-                    Id = brand.Id,
-                    BrandName = brand.BrandName,
-                    CreatedBy = new Guid(Userid),
-                    CreatedTime = DateTime.Now,
-                    UpdatedBy = Guid.Empty,
-                    UpdatedTime = null,
-                    Status = false
-                };
-                _context.Add(b);
+                _context.Add(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(brand);
+            return View(category);
         }
 
-        // GET: Admin/Brands/Edit/5
+        // GET: Admin/Categories/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -91,27 +80,22 @@ namespace VRS.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var brand = await _context.brands.FindAsync(id);
-            if (brand == null)
+            var category = await _context.categories.FindAsync(id);
+            if (category == null)
             {
                 return NotFound();
             }
-            BrandVM brandVM = new BrandVM
-            {
-                BrandName = brand.BrandName,
-                Id = brand.Id
-            };
-            return View(brandVM);
+            return View(category);
         }
 
-        // POST: Admin/Brands/Edit/5
+        // POST: Admin/Categories/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit([Bind("Id,BrandName")] BrandVM brand)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CategoryName,Status,CreatedBy,CreatedTime,UpdatedBy,UpdatedTime")] Category category)
         {
-            if (brand.Id == 0)
+            if (id != category.Id)
             {
                 return NotFound();
             }
@@ -120,25 +104,12 @@ namespace VRS.Areas.Admin.Controllers
             {
                 try
                 {
-                    // For ASP.NET Core >= 5.0
-                    var Userid = User.FindFirstValue(ClaimTypes.NameIdentifier);// will give the user's Id
-
-                    var b = await _context.brands.FindAsync(brand.Id);
-                    if (b == null)
-                    {
-                        return NotFound();
-                    }
-                    b.BrandName = brand.BrandName;
-                    b.UpdatedBy = new Guid(Userid);
-                    b.UpdatedTime = DateTime.Now;
-
-
-                    _context.Update(b);
+                    _context.Update(category);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!BrandExists(brand.Id))
+                    if (!CategoryExists(category.Id))
                     {
                         return NotFound();
                     }
@@ -149,10 +120,10 @@ namespace VRS.Areas.Admin.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(brand);
+            return View(category);
         }
 
-        // GET: Admin/Brands/Delete/5
+        // GET: Admin/Categories/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -160,34 +131,34 @@ namespace VRS.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var brand = await _context.brands
+            var category = await _context.categories
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (brand == null)
+            if (category == null)
             {
                 return NotFound();
             }
 
-            return View(brand);
+            return View(category);
         }
 
-        // POST: Admin/Brands/Delete/5
+        // POST: Admin/Categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var brand = await _context.brands.FindAsync(id);
-            if (brand != null)
+            var category = await _context.categories.FindAsync(id);
+            if (category != null)
             {
-                _context.brands.Remove(brand);
+                _context.categories.Remove(category);
             }
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool BrandExists(int id)
+        private bool CategoryExists(int id)
         {
-            return _context.brands.Any(e => e.Id == id);
+            return _context.categories.Any(e => e.Id == id);
         }
     }
 }
---
+++ VRS/Areas/Admin/Controllers/VehiclesController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VRS.Areas.Admin.Models;
+using VRS.Data;
+
+namespace VRS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class VehiclesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VehiclesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Vehicles
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.vehicles.Include(v => v.Brand).Include(v => v.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Admin/Vehicles/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = await _context.vehicles
+                .Include(v => v.Brand)
+                .Include(v => v.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicle);
+        }
+
+        // GET: Admin/Vehicles/Create
+        public IActionResult Create()
+        {
+            ViewData["BrandId"] = new SelectList(_context.brands, "Id", "BrandName");
+            ViewData["CategoryId"] = new SelectList(_context.categories, "Id", "CategoryName");
+            return View();
+        }
+
+        // POST: Admin/Vehicles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CategoryId,BrandId,VehicleName,ModelName,ModelYear,Color,Seats,Price,RentPerHour,VehicleImage,Availability,CreatedBy,CreatedTime,UpdatedBy,UpdatedTime")] Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vehicle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.brands, "Id", "BrandName", vehicle.BrandId);
+            ViewData["CategoryId"] = new SelectList(_context.categories, "Id", "CategoryName", vehicle.CategoryId);
+            return View(vehicle);
+        }
+
+        // GET: Admin/Vehicles/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = await _context.vehicles.FindAsync(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            ViewData["BrandId"] = new SelectList(_context.brands, "Id", "BrandName", vehicle.BrandId);
+            ViewData["CategoryId"] = new SelectList(_context.categories, "Id", "CategoryName", vehicle.CategoryId);
+            return View(vehicle);
+        }
+
+        // POST: Admin/Vehicles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,CategoryId,BrandId,VehicleName,ModelName,ModelYear,Color,Seats,Price,RentPerHour,VehicleImage,Availability,CreatedBy,CreatedTime,UpdatedBy,UpdatedTime")] Vehicle vehicle)
+        {
+            if (id != vehicle.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehicle);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleExists(vehicle.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.brands, "Id", "BrandName", vehicle.BrandId);
+            ViewData["CategoryId"] = new SelectList(_context.categories, "Id", "CategoryName", vehicle.CategoryId);
+            return View(vehicle);
+        }
+
+        // GET: Admin/Vehicles/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = await _context.vehicles
+                .Include(v => v.Brand)
+                .Include(v => v.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicle);
+        }
+
+        // POST: Admin/Vehicles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var vehicle = await _context.vehicles.FindAsync(id);
+            if (vehicle != null)
+            {
+                _context.vehicles.Remove(vehicle);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleExists(long id)
+        {
+            return _context.vehicles.Any(e => e.Id == id);
+        }
+    }
+}
